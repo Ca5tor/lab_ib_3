dd603f4b9d67aed96173324c0c7c5b70728eb708
Ca5tor <48414173+Ca5tor@users.noreply.github.com>
2021-03-16 12:12:44 +0300

    готовая программа, без проверок на ввод

--- lab_ib_3/MyForm.cpp
+++ lab_ib_3/MyForm.cpp
@@ -1,5 +1,6 @@
 #include "MyForm.h"
 #include <Windows.h>
+#include <cmath>
 
 using namespace System;
 using namespace System::Windows::Forms;
@@ -15,28 +16,53 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	return 0;
 }
 
-// ѕроверка что поле не пустое
-bool InputValidation(TextBox^ Tx, String^ s, double& d) {
-	if (Tx->Text->Length == 0) {
-		MessageBox::Show(s, "Error!", MessageBoxButtons::OK, MessageBoxIcon::Error);
-		Tx->Focus();
-		return false;
-	}
-	d = Convert::ToDouble(Tx->Text);
-
-	return true;
+String^ StringConcatenation(String^ pas, String^ s, CheckBox^ cb) {
+	cb->Checked == true ? pas += s : pas = pas;
+	return pas;
 }
 
 System::Void labib3::MyForm::button1_Click(System::Object^ sender, System::EventArgs^ e){
 	
-	double P = 0., V = 0., T = 0.;
+	double p = Convert::ToDouble(textBox1->Text);
+	int v = Convert::ToInt32(textBox2->Text);
+	int t = Convert::ToInt32(textBox3->Text);
+	
+	// ѕереведЄм недели в мин, домножив на: кол-во дней в нед = 7, кол-во часов в 1 дне = 24 и кол-во мин в 1 часе = 60 
+	t *= 10080;
 
-	if (!InputValidation(textBox1, "¬ведите значение P", P)) return;
-	if (!InputValidation(textBox2, "¬ведите значение V", V)) return;
-	if (!InputValidation(textBox3, "¬ведите значение T", T)) return;
+	String^ BigRu = "јЅ¬√ƒ≈®∆«»… ЋћЌќѕ–—“”‘’÷„ЎўЏџ№Ёёя";
+	String^ SmallRu = "абвгдеЄжзийклмнопрстуфхцчшщъыьэю€";
+	String^ BigEng = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	String^ SmallEng = "abcdefghijklmnopqrstuvwxyz";
+	String^ Symbols = "!@#$%^&*()-_=+:;є.,\"\'?<>\|\\\/";
+	String^ Numbers = "123456789";
+	String^ SymbolsToUse = ""; // —трока где будут допущенные символы дл€ генерации парол€
+	String^ Password = "";
 
+	// ƒобавл€ем в общую строку те символы которые выбрали через checkBox
+	SymbolsToUse = StringConcatenation(SymbolsToUse, BigEng,   checkBox1);
+	SymbolsToUse = StringConcatenation(SymbolsToUse, SmallEng, checkBox2);
+	SymbolsToUse = StringConcatenation(SymbolsToUse, BigRu,    checkBox3);
+	SymbolsToUse = StringConcatenation(SymbolsToUse, SmallRu,  checkBox4);
+	SymbolsToUse = StringConcatenation(SymbolsToUse, Symbols,  checkBox5);
+	SymbolsToUse = StringConcatenation(SymbolsToUse, Numbers,  checkBox6);
 
+	double s = ceil(double(v * t) / p); // —читаем s с округлением вверх
+	label7->Text = s.ToString();
+
+	int a = SymbolsToUse->Length; //  оличество символов, которые могут быть использованы при составлении парол€
+	label8->Text = a.ToString();
+
+	// »спользу€ сво-во log получаем значение L(длина парол€) с округлением до ближайшего целого числа
+	int l = ceil(log(double(v * t) / p) / log(a));
+	label9->Text = l.ToString();
+
+	
+	s <= pow(a, l) ? label12->Text = "True" : label12->Text = "False";
+
+	for (int i = 0; i != l; i++) Password += SymbolsToUse[rand() % a];
+	
+	label10->Text = "ѕароль: " + Password->Replace("&", "&&");
 
 	return System::Void();
 }
-
--- lab_ib_3/MyForm.h
+++ lab_ib_3/MyForm.h
@@ -58,6 +58,8 @@ namespace labib3 {
 	private: System::Windows::Forms::CheckBox^ checkBox3;
 	private: System::Windows::Forms::CheckBox^ checkBox2;
 	private: System::Windows::Forms::Label^ label10;
+	private: System::Windows::Forms::Label^ label11;
+	private: System::Windows::Forms::Label^ label12;
 
 	private:
 		/// <summary>
@@ -77,24 +79,26 @@ namespace labib3 {
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
-			this->panel2 = (gcnew System::Windows::Forms::Panel());
-			this->panel3 = (gcnew System::Windows::Forms::Panel());
-			this->checkBox2 = (gcnew System::Windows::Forms::CheckBox());
-			this->checkBox3 = (gcnew System::Windows::Forms::CheckBox());
-			this->checkBox4 = (gcnew System::Windows::Forms::CheckBox());
-			this->checkBox5 = (gcnew System::Windows::Forms::CheckBox());
-			this->checkBox6 = (gcnew System::Windows::Forms::CheckBox());
-			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label9 = (gcnew System::Windows::Forms::Label());
+			this->label8 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
-			this->label4 = (gcnew System::Windows::Forms::Label());
-			this->label5 = (gcnew System::Windows::Forms::Label());
-			this->label6 = (gcnew System::Windows::Forms::Label());
-			this->label7 = (gcnew System::Windows::Forms::Label());
-			this->label8 = (gcnew System::Windows::Forms::Label());
-			this->label9 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->checkBox6 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox5 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox4 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox3 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox2 = (gcnew System::Windows::Forms::CheckBox());
+			this->panel3 = (gcnew System::Windows::Forms::Panel());
 			this->label10 = (gcnew System::Windows::Forms::Label());
+			this->label11 = (gcnew System::Windows::Forms::Label());
+			this->label12 = (gcnew System::Windows::Forms::Label());
 			this->panel1->SuspendLayout();
 			this->panel2->SuspendLayout();
 			this->panel3->SuspendLayout();
@@ -156,6 +160,98 @@ namespace labib3 {
 			this->panel1->Size = System::Drawing::Size(325, 158);
 			this->panel1->TabIndex = 4;
 			// 
+			// label9
+			// 
+			this->label9->AutoSize = true;
+			this->label9->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label9->Location = System::Drawing::Point(173, 130);
+			this->label9->Name = L"label9";
+			this->label9->Size = System::Drawing::Size(14, 13);
+			this->label9->TabIndex = 15;
+			this->label9->Text = L"0";
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label8->Location = System::Drawing::Point(173, 107);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(14, 13);
+			this->label8->TabIndex = 14;
+			this->label8->Text = L"0";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label7->Location = System::Drawing::Point(173, 84);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(14, 13);
+			this->label7->TabIndex = 13;
+			this->label7->Text = L"0";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(3, 130);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(91, 13);
+			this->label6->TabIndex = 12;
+			this->label6->Text = L"L (длина пароля)";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(3, 107);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(127, 13);
+			this->label5->TabIndex = 10;
+			this->label5->Text = L"A (мощность алфавита)";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(3, 84);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(154, 13);
+			this->label4->TabIndex = 8;
+			this->label4->Text = L"S* (нижняя граница паролей)";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(3, 61);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(169, 13);
+			this->label3->TabIndex = 6;
+			this->label3->Text = L"T (срок действия пароля в нед.)";
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(176, 57);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(137, 20);
+			this->textBox3->TabIndex = 7;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(3, 38);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(153, 13);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"V (скорость перебора в мин)";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(176, 34);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(137, 20);
+			this->textBox2->TabIndex = 5;
+			// 
 			// panel2
 			// 
 			this->panel2->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
@@ -170,45 +266,15 @@ namespace labib3 {
 			this->panel2->Size = System::Drawing::Size(159, 158);
 			this->panel2->TabIndex = 5;
 			// 
-			// panel3
+			// checkBox6
 			// 
-			this->panel3->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
-			this->panel3->Controls->Add(this->label10);
-			this->panel3->Controls->Add(this->button1);
-			this->panel3->Location = System::Drawing::Point(12, 176);
-			this->panel3->Name = L"panel3";
-			this->panel3->Size = System::Drawing::Size(490, 66);
-			this->panel3->TabIndex = 6;
-			// 
-			// checkBox2
-			// 
-			this->checkBox2->AutoSize = true;
-			this->checkBox2->Location = System::Drawing::Point(12, 34);
-			this->checkBox2->Name = L"checkBox2";
-			this->checkBox2->Size = System::Drawing::Size(140, 17);
-			this->checkBox2->TabIndex = 1;
-			this->checkBox2->Text = L"Латинские маленькие";
-			this->checkBox2->UseVisualStyleBackColor = true;
-			// 
-			// checkBox3
-			// 
-			this->checkBox3->AutoSize = true;
-			this->checkBox3->Location = System::Drawing::Point(12, 57);
-			this->checkBox3->Name = L"checkBox3";
-			this->checkBox3->Size = System::Drawing::Size(115, 17);
-			this->checkBox3->TabIndex = 2;
-			this->checkBox3->Text = L"Русские большие";
-			this->checkBox3->UseVisualStyleBackColor = true;
-			// 
-			// checkBox4
-			// 
-			this->checkBox4->AutoSize = true;
-			this->checkBox4->Location = System::Drawing::Point(12, 80);
-			this->checkBox4->Name = L"checkBox4";
-			this->checkBox4->Size = System::Drawing::Size(127, 17);
-			this->checkBox4->TabIndex = 3;
-			this->checkBox4->Text = L"Русские маленькие";
-			this->checkBox4->UseVisualStyleBackColor = true;
+			this->checkBox6->AutoSize = true;
+			this->checkBox6->Location = System::Drawing::Point(12, 126);
+			this->checkBox6->Name = L"checkBox6";
+			this->checkBox6->Size = System::Drawing::Size(62, 17);
+			this->checkBox6->TabIndex = 5;
+			this->checkBox6->Text = L"Цифры";
+			this->checkBox6->UseVisualStyleBackColor = true;
 			// 
 			// checkBox5
 			// 
@@ -220,107 +286,45 @@ namespace labib3 {
 			this->checkBox5->Text = L"Символы";
 			this->checkBox5->UseVisualStyleBackColor = true;
 			// 
-			// checkBox6
+			// checkBox4
 			// 
-			this->checkBox6->AutoSize = true;
-			this->checkBox6->Location = System::Drawing::Point(12, 126);
-			this->checkBox6->Name = L"checkBox6";
-			this->checkBox6->Size = System::Drawing::Size(62, 17);
-			this->checkBox6->TabIndex = 5;
-			this->checkBox6->Text = L"Цифры";
-			this->checkBox6->UseVisualStyleBackColor = true;
+			this->checkBox4->AutoSize = true;
+			this->checkBox4->Location = System::Drawing::Point(12, 80);
+			this->checkBox4->Name = L"checkBox4";
+			this->checkBox4->Size = System::Drawing::Size(127, 17);
+			this->checkBox4->TabIndex = 3;
+			this->checkBox4->Text = L"Русские маленькие";
+			this->checkBox4->UseVisualStyleBackColor = true;
 			// 
-			// label2
+			// checkBox3
 			// 
-			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(3, 38);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(121, 13);
-			this->label2->TabIndex = 4;
-			this->label2->Text = L"V (скорость перебора)";
+			this->checkBox3->AutoSize = true;
+			this->checkBox3->Location = System::Drawing::Point(12, 57);
+			this->checkBox3->Name = L"checkBox3";
+			this->checkBox3->Size = System::Drawing::Size(115, 17);
+			this->checkBox3->TabIndex = 2;
+			this->checkBox3->Text = L"Русские большие";
+			this->checkBox3->UseVisualStyleBackColor = true;
 			// 
-			// textBox2
+			// checkBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(176, 34);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(137, 20);
-			this->textBox2->TabIndex = 5;
+			this->checkBox2->AutoSize = true;
+			this->checkBox2->Location = System::Drawing::Point(12, 34);
+			this->checkBox2->Name = L"checkBox2";
+			this->checkBox2->Size = System::Drawing::Size(140, 17);
+			this->checkBox2->TabIndex = 1;
+			this->checkBox2->Text = L"Латинские маленькие";
+			this->checkBox2->UseVisualStyleBackColor = true;
 			// 
-			// label3
+			// panel3
 			// 
-			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(3, 61);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(136, 13);
-			this->label3->TabIndex = 6;
-			this->label3->Text = L"T (срок действия пароля)";
-			// 
-			// textBox3
-			// 
-			this->textBox3->Location = System::Drawing::Point(176, 57);
-			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(137, 20);
-			this->textBox3->TabIndex = 7;
-			// 
-			// label4
-			// 
-			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(3, 84);
-			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(154, 13);
-			this->label4->TabIndex = 8;
-			this->label4->Text = L"S* (нижняя граница паролей)";
-			// 
-			// label5
-			// 
-			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(3, 107);
-			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(127, 13);
-			this->label5->TabIndex = 10;
-			this->label5->Text = L"A (мощность алфавита)";
-			// 
-			// label6
-			// 
-			this->label6->AutoSize = true;
-			this->label6->Location = System::Drawing::Point(3, 130);
-			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(91, 13);
-			this->label6->TabIndex = 12;
-			this->label6->Text = L"L (длина пароля)";
-			// 
-			// label7
-			// 
-			this->label7->AutoSize = true;
-			this->label7->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label7->Location = System::Drawing::Point(173, 84);
-			this->label7->Name = L"label7";
-			this->label7->Size = System::Drawing::Size(14, 13);
-			this->label7->TabIndex = 13;
-			this->label7->Text = L"0";
-			// 
-			// label8
-			// 
-			this->label8->AutoSize = true;
-			this->label8->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label8->Location = System::Drawing::Point(173, 107);
-			this->label8->Name = L"label8";
-			this->label8->Size = System::Drawing::Size(14, 13);
-			this->label8->TabIndex = 14;
-			this->label8->Text = L"0";
-			// 
-			// label9
-			// 
-			this->label9->AutoSize = true;
-			this->label9->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->label9->Location = System::Drawing::Point(173, 130);
-			this->label9->Name = L"label9";
-			this->label9->Size = System::Drawing::Size(14, 13);
-			this->label9->TabIndex = 15;
-			this->label9->Text = L"0";
+			this->panel3->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->panel3->Controls->Add(this->label10);
+			this->panel3->Controls->Add(this->button1);
+			this->panel3->Location = System::Drawing::Point(12, 214);
+			this->panel3->Name = L"panel3";
+			this->panel3->Size = System::Drawing::Size(490, 66);
+			this->panel3->TabIndex = 6;
 			// 
 			// label10
 			// 
@@ -333,10 +337,34 @@ namespace labib3 {
 			this->label10->TabIndex = 14;
 			this->label10->Text = L"Пароль:";
 			// 
+			// label11
+			// 
+			this->label11->AutoSize = true;
+			this->label11->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label11->Location = System::Drawing::Point(17, 182);
+			this->label11->Name = L"label11";
+			this->label11->Size = System::Drawing::Size(265, 16);
+			this->label11->TabIndex = 15;
+			this->label11->Text = L"Выполнение условия S* <= S = A^L:";
+			// 
+			// label12
+			// 
+			this->label12->AutoSize = true;
+			this->label12->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label12->Location = System::Drawing::Point(280, 182);
+			this->label12->Name = L"label12";
+			this->label12->Size = System::Drawing::Size(13, 16);
+			this->label12->TabIndex = 15;
+			this->label12->Text = L"-";
+			// 
 			// MyForm
 			// 
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
-			this->ClientSize = System::Drawing::Size(513, 254);
+			this->ClientSize = System::Drawing::Size(513, 304);
+			this->Controls->Add(this->label12);
+			this->Controls->Add(this->label11);
 			this->Controls->Add(this->panel3);
 			this->Controls->Add(this->panel2);
 			this->Controls->Add(this->panel1);
@@ -351,6 +379,7 @@ namespace labib3 {
 			this->panel3->ResumeLayout(false);
 			this->panel3->PerformLayout();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
